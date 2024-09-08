3e780a4467661f5b84a340de726d1be6a1347b04
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-09 00:48:19 +0530

    need to impoert Link

--- src/component/Navbar/Nvabar.js
+++ src/component/Navbar/Nvabar.js
@@ -3,6 +3,7 @@ import "./Nvabar.css"
 import { Link } from "react-router-dom";
 
 
+
 function Navbar()
 {
     return (
@@ -10,6 +11,7 @@ function Navbar()
       kuch meetha🍫ho jaye 
 
     <div className="navbar_links ">
+    {/* "Link comopnent is used as anchor tag in react router dom laibery href is replaced with to  */}
        <Link to="/"> Home</Link>
        <Link to="/About" > About</Link>
        <Link to="/Contact" > Contact</Link>
