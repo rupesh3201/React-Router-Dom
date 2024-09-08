ecfb3bc8cc6a3277b8c1c55b71ea8acaae20959c
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-08 22:23:58 +0530

    Foldeer structure creatted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-
+import ReactDOM from 'react-dom/client'
+import Home from './views/Home/Home'
+import About from './views/About/About';
+import Contact from './views/Contact/Contact';
+import "./index.css"
+const current_path = window.location.pathname
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    Hellow guys
-
-  </div>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
