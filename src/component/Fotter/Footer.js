15e4e806b733663daa4413b2f2ac1338d7e67457
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-08 22:29:16 +0530

    added fotter

---
+++ src/component/Fotter/Footer.js
@@ -0,0 +1,10 @@
+import "./Fotter.css"
+// improted css
+import React  from "react";
+function Footter()
+{
+    return (
+        <div className="footer">All reserved copyrights & copy:2024</div>
+    )
+}
+export default Footter
