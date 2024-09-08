4a8f630636b77023d901192c2194fceb58e9fe42
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-08 22:27:16 +0530

    Nvbar

---
+++ src/component/Navbar/Nvabar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css"
+
+
+function Navbar()
+{
+    return (
+       <div className="navbar">
+      kuch meetha🍫ho jaye 
+
+    <div className="navbar_links ">
+       <a href="/"> Home</a>
+       <a href="/About" > About</a>
+       <a href="/Contact" > Contact</a>
+       </div>
+       </div>
+    )
+}
+export default Navbar
